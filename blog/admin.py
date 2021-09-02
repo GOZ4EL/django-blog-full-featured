6d1dff6b46d3150dc8afbcd2afd869b65b0b5745
goz4el <hordemzerado@gmail.com>
2021-09-01 22:58:13 -0400

    Changing the way the Post model is displayed on the administration site.

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,13 @@ This module defines which models can be used on the administration site.
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
 
