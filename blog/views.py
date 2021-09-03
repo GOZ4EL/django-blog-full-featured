74d4887acd2287605e925ef10eb7e7fcc760ffe9
goz4el <hordemzerado@gmail.com>
2021-09-03 19:16:46 -0400

    Adding pagination.

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,32 @@
 This module contains all the views logic of the post model.
 """
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import(
+    Paginator,
+    EmptyPage,
+    PageNotAnInteger
+)
+
 from .models import Post
+
 
 def post_list(request):
     """
     Render all published posts.
     """
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
