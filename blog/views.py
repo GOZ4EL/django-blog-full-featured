158959a9c9cab9b20a0197e2c525991225f44e99
goz4el <hordemzerado@gmail.com>
2021-09-02 21:59:17 -0400

    Creating list and detail views for the Post model.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+"""
+This module contains all the views logic of the post model.
+"""
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    """
+    Render all published posts.
+    """
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    """
+    Render the details of a specific post.
+    """
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+
