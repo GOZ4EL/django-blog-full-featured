c78beb4a15eb37bb1919f7c9eeadf196c7a629d6
goz4el <hordemzerado@gmail.com>
2021-09-03 21:21:55 -0400

    Fixing typo.

--- blog/forms.py
+++ blog/forms.py
@@ -9,5 +9,5 @@ class EmailPostForm(forms.Form):
     email = forms.EmailField()
     to = forms.EmailField() 
     comments = forms.CharField(required=False,
-                               widgets=forms.Textarea)
+                               widget=forms.Textarea)
 
