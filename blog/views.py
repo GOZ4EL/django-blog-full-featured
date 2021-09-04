5424d7d35c0399e45852f4272ff366caf239769a
goz4el <hordemzerado@gmail.com>
2021-09-03 20:54:02 -0400

    Adding email sender to the post form handler.

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from django.core.paginator import(
     PageNotAnInteger
 )
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
 from .forms import EmailPostForm
@@ -35,10 +36,20 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build.absolute_uri(
+                post.get.absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\s comments: {cd['comments']}"
+            send_mail(subject, message, 'hordemzerado@gmail.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
