482d0eb95f493bdd9392d8f7b239b75bcdc4c5f6
goz4el <hordemzerado@gmail.com>
2021-09-05 11:06:30 -0400

    Resolving new line problems

--- blog/views.py
+++ blog/views.py
@@ -72,7 +72,6 @@ def post_detail(request, year, month, day, post):
     similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
                                 .order_by('-same_tags', '-publish') [:4]
 
-
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
