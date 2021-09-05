ba7a4b787888632ea212df3e3cc273e23c2a8cac
goz4el <hordemzerado@gmail.com>
2021-09-04 22:18:23 -0400

    Making the post list view capable of showing posts by tag

--- blog/views.py
+++ blog/views.py
@@ -10,15 +10,23 @@ from django.core.paginator import(
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
+from taggit.models import Tag
+
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     """
     Render all published posts.
     """
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -30,7 +38,8 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})
+                   'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
